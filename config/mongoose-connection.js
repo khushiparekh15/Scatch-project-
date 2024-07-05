4660a3f035f7a6622c9d65a2499fdb15edcdaad7
unknown <khushiparekh1507@gmail.com>
2024-07-05 22:35:56 +1200

    format files

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/scatch")
+const mongoose = require("mongoose");
+const config = require("config");
+const dbgr = require("debug")("development:mongoose");
+
+
+mongoose.connect(`${config.get("MONGODB_URL")}/scatch`)
 .then(function(){
-    console.log("Connected");
+    dbgr("Connected");
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
